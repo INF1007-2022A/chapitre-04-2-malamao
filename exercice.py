b157def8dac9c1680854ef6144b96c6927208a36
malamao <marie-catherine.lamy@polymtl.ca>
2022-09-19 23:12:32 -0400

    ex 1 et 2

--- exercice.py
+++ exercice.py
@@ -4,10 +4,24 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	#aller chercher premier nom
+	premier_nom = name.split("-")[0]
+	#mettre la première lettre en majuscule et le reste en minuscules
+	case = premier_nom[0].upper()+premier_nom[1:].lower()
+
+	return "Bonjour "+case
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	#choisir animal
+	emplacement=random.randint(0, 2)
+	animal=animals[emplacement]
+	#choisir adjectif
+	emplacement = random.randint(0, 2)
+	adjectif=adjectives[emplacement]
+	#choisir fruit
+	emplacement = random.randint(0, 2)
+	fruit=fruits[emplacement]
+	return f"Aujourd’hui, j’ai vu un {animal} s’emparer d’un panier {adjectif} plein de {fruit}."
 
 def encrypt(text, shift):
 	return ""
